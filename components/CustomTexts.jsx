cbd0b3c8885b949631056241d3d4ebaf6a12915f
Sara Mohammed <sara.korea96@gmail.com>
2023-02-05 13:41:25 +0300

    deploy the app changes

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,8 +1,9 @@
 'use client';
-import { motion } from 'framer-motion';
-import { textContainer, textVariant2 } from "../utils/motion";
 
-export const TypingText = ({title, textStyles}) => (
+import { motion } from 'framer-motion';
+import { textContainer, textVariant2 } from '../utils/motion';
+
+export const TypingText = ({ title, textStyles }) => (
   <motion.p
     variants={textContainer}
     className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
@@ -16,7 +17,7 @@ export const TypingText = ({title, textStyles}) => (
   </motion.p>
 );
 
-export const TitleText = ({title, textStyles}) => (
+export const TitleText = ({ title, textStyles }) => (
   <motion.h2
     variants={textVariant2}
     initial="hidden"
--- sections/About.jsx
+++ sections/About.jsx
@@ -8,19 +8,9 @@ import { fadeIn, staggerContainer } from '../utils/motion';
 const About = () => (
   <section className={`${styles.paddings} relative z-10`}>
     <div className="gradient-02 z-0" />
-      <motion.div
-        variants={staggerContainer}
-        initial="hidden"
-        whileInView="show"
-        viewport={{ once: false, amount: 0.25 }}
-        className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
-      >
+      <motion.div variants={staggerContainer} initial="hidden" whileInView="show" viewport={{ once: false, amount: 0.25 }} className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`} >
         <TypingText title="|About Metaversus" textStyles="text-center" />
-
-        <motion.p
-        variants={fadeIn('up', 'tween', 0.2, 1)}
-        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
-      >
+         <motion.p variants={fadeIn('up', 'tween', 0.2, 1)} className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white">
         <span className="font-extrabold text-white">Metaverse</span> is a new
         thing in the future, where you can enjoy the virtual world by feeling
         like it's really real, you can feel what you feel in this metaverse
